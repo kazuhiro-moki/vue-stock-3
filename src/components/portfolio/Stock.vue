4663db771cf7de2e0e19d576d51f8401da8a7ba0
moki.kazuhiro <futuredictator30@yahoo.co.jp>
2020-07-14 02:21:32 +0900

    add some order checks

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -14,20 +14,27 @@
             class="form-control" 
             placeholder="Quantity"
             v-model="quantity"
+            :class="{danger: insufficientQuantity}"
             >
         </div>
         <div class="pull-right">
           <button 
             class="btn btn-success"
             @click="sellStock"
-            :disabled="quantity <= 0"
-            >Sell
+            :disabled="insufficientQuantity || quantity <= 0"
+            >{{ insufficientQuantity ? 'Not enough' : 'Sell' }}
             </button>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+  .danger {
+    border: 1px solid red;
+  }
+</style>
 
 <script>
 import {mapActions} from 'vuex';
@@ -37,6 +44,11 @@ export default {
   data() {
     return {
       quantity: 0
+    }
+  },
+  computed: {
+    insufficientQuantity() {
+      return this.quantity > this.stock.quantity;
     }
   },
   methods: {
