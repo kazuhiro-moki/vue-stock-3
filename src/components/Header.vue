c4a66d9750909ed6769fe1f4567bac34d91a2f77
moki.kazuhiro <futuredictator30@yahoo.co.jp>
2020-07-14 20:09:13 +0900

    save data

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -27,7 +27,7 @@
               >Save & Load <span class="caret"></span
             ></a>
             <ul class="dropdown-menu">
-              <li><a href="#">Save Data</a></li>
+              <li><a href="#" @click="saveData">Save Data</a></li>
               <li><a href="#">Load Data</a></li>
             </ul>
           </li>
@@ -58,6 +58,14 @@ export default {
     ]),
     endDay() {
       this.randomizeStocks();
+    },
+    saveData() {
+      const data = {
+        funds: this.$store.getters.funds,
+        stockPortfolio: this.$store.getters.stockPortfolio,
+        stocks: this.$store.getters.stocks
+      };
+      this.$http.put('data.json', data);
     }
   }
 }
